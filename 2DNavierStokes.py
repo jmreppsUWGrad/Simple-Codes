3c14c71ed6b6621e205b1db1e0f432013f548681
J. Mark Epps <jmrepps@edu.uwaterloo.ca>
2018-09-15 16:39:34 -0400

    Recode and finish Poisson pressure function incl 0 pressure gradient BCs

--- 2DNavierStokes.py
+++ 2DNavierStokes.py
@@ -22,7 +22,7 @@ Function inputs:
     dxyt: array with dx, dy and dt respectively
     prop: array with fluid properties rho and mu respectively
     conv: convergence criteria
-    dp_zero: array indicating which boundaries have 0 pressure gradients (normal to boundary)
+    dp_zero: array indicating which boundaries have 0 pressure gradients (normal to that boundary)
     
 
 """
@@ -42,50 +42,208 @@ def ResolvePress(p, u, v, dxyt, prop, conv, dp_zero):
     pn=p.copy()
     while (diff>conv) and (count<1000):
         
-        # Poisson equation for pressure (first nodes inside boundary; small x) FIX v terms in last line
-        pn[1:-1,1]=(dy**2*(p[2:,1]+p[:-2,1])+dx**2*(p[1:-1,2]+p[1:-1,0]))/(2*(dx**2+dy**2)) \
-          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[1:-1,2]-u[1:-1,0])**2+rho*dx**2/4*(v[2:,1]-v[:-2,1])**2 \
-          +rho*dx*dy/2*(u[2:,1]-u[:-2,1])*(v[1:-1,2]-v[1:-1,0])\
-          +u[1:-1,2]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[1:-1,1]+dx*mu-3*dy**2/dx*mu)\
-          +u[1:-1,0]*(rho*dx*dy**2/2/dt+rho*dy**2*u[1:-1,1]-dx*mu-dy**2/dx*mu)\
-          +u[2:,2]*(rho*dy*dx/4*v[1:-1,2]-dx*mu/2) \
-          +u[2:,0]*(-rho*dx*dy/4*v[2:,1]-dx*mu/2) \
-          +u[:-2,2]*(-rho*dy*dx/4*v[:-2,1]+dx*mu/2) \
-          +u[:-2,0]*(rho*dx*dy/4*v[1:-1,0]+dx*mu/2) \
-          -2*rho*dy**2*u[1:-1,1]**2 \
-          +dy**2/dx*mu*u[1:-1,1]- dy**2*mu/dx*(u[1:-1,4]-u[1:-1,3]) \
-          +v[2:,2]*(rho*dx*dy/4*u[1:-1,2]-dy*mu/2) \
-          +v[:-2,2]*(-rho*dx*dy/4*u[1:-1,2]+dy*mu/2) \
-          +v[2:,0]*(-rho*dx*dy/4*u[1:-1,0]-dy*mu/2) \
-          +v[:-2,0]*(rho*dx*dy/4*u[1:-1,0]+dy*mu/2) \
-          +v[2:,1]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[1:-1,1]+dy*mu-dx**2/dy*mu) \
-          +v[:-2,1]*(rho*dx**2*dy/2/dt+rho*dx**2*v[1:-1,1]-dy*mu-dx**2/dy*mu) \
-          -2*dx**2*v[1:-1,1]**2 \
-          +dx**2/dy*mu*v[1:-1,1]- dx**2/dy*mu*(v[4:,1]-v[3:,1]))
-          
-        # Poisson equation for pressure (first nodes inside boundary; large x)
-        pn[1:-1,-2]=(dy**2*(p[2:,-2]+p[:-2,-2])+dx**2*(p[1:-1,-1]+p[1:-1,-3]))/(2*(dx**2+dy**2)) \
-          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[1:-1,-1]-u[1:-1,-3])**2+rho*dx**2/4*(v[2:,-2]-v[:-2,-2])**2 \
-          +rho*dx*dy/2*(u[2:,-2]-u[-3,-2])*(v[1:-1,-1]-v[1:-1,-3])\
-          +u[1:-1,-1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[1:-1,-2]+dx*mu-3*dy**2/dx*mu)\
-          +u[1:-1,-3]*(rho*dx*dy**2/2/dt+rho*dy**2*u[1:-1,-2]-dx*mu-dy**2/dx*mu)\
-          +u[2:,-1]*(rho*dy*dx/4*v[1:-1,-1]-dx*mu/2) \
-          +u[2:,-3]*(-rho*dx*dy/4*v[2:,-2]-dx*mu/2) \
-          +u[:-2,-1]*(-rho*dy*dx/4*v[:-2,-2]+dx*mu/2) \
-          +u[:-2,-3]*(rho*dx*dy/4*v[1:-1,-3]+dx*mu/2) \
-          -2*rho*dy**2*u[1:-1,-2]**2 \
-          +dy**2/dx*mu*u[1:-1,-2]- dy**2*mu/dx*(u[1:-1,4]-u[1:-1,3]) \
-          +v[2:,-1]*(rho*dx*dy/4*u[1:-1,-1]-dy*mu/2) \
-          +v[:-2,-1]*(-rho*dx*dy/4*u[1:-1,-1]+dy*mu/2) \
-          +v[2:,-3]*(-rho*dx*dy/4*u[1:-1,-3]-dy*mu/2) \
-          +v[:-2,-3]*(rho*dx*dy/4*u[1:-1,-3]+dy*mu/2) \
-          +v[2:,-2]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[1:-1,-2]+dy*mu-dx**2/dy*mu) \
-          +v[:-2,-2]*(rho*dx**2*dy/2/dt+rho*dx**2*v[1:-1,-2]-dy*mu-dx**2/dy*mu) \
-          -2*dx**2*v[1:-1,-2]**2 \
-          +dx**2/dy*mu*v[1:-1,-2]- dx**2/dy*mu*(v[4,1:-1]-v[3,1:-1]))  
-        # Poisson equation for pressure (first nodes inside boundary; small y)
-          
+        # Poisson equation for pressure (first nodes inside boundary; small x)
+        st=1
+        en=-3
+        sin=1
+        fdu=1
+        fdv=1
+        pn[st:en,sin]=(dy**2*(p[st+1:en+1,sin]+p[st-1:en-1,sin])+dx**2*(p[st:en,sin+1]+p[st:en,sin-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[st:en,sin+1]-u[st:en,sin-1])**2+rho*dx**2/4*(v[st+1:en+1,sin]-v[st-1:en-1,sin])**2 \
+          +rho*dx*dy/2*(u[st+1:en+1,sin]-u[st-1:en-1,sin])*(v[st:en,sin+1]-v[st:en,sin-1])\
+          +u[st:en,sin+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]+dx*mu)\
+          +u[st:en,sin-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]-dx*mu-dy**2/dx*mu)\
+          +u[st+1:en+1,sin+1]*(rho*dy*dx/4*v[st:en,sin+1]-dx*mu/2) \
+          +u[st+1:en+1,sin-1]*(-rho*dx*dy/4*v[st+1:en+1,sin]-dx*mu/2) \
+          +u[st-1:en-1,sin+1]*(-rho*dy*dx/4*v[st-1:en-1,sin]+dx*mu/2) \
+          +u[st-1:en-1,sin-1]*(rho*dx*dy/4*v[st:en,sin-1]+dx*mu/2) \
+          -2*rho*dy**2*u[st:en,sin]**2 \
+          +fdu*(dy**2/dx*mu*u[st:en,sin] -3*dy**2/dx*mu*u[st:en,sin+fdu]\
+          - dy**2*mu/dx*(u[st:en,sin+fdu*3]-3*u[st:en,sin+fdu*2])) \
+          +v[st+1:en+1,sin+1]*(rho*dx*dy/4*u[st:en,sin+1]-dy*mu/2) \
+          +v[st-1:en-1,sin+1]*(-rho*dx*dy/4*u[st:en,sin+1]+dy*mu/2) \
+          +v[st+1:en+1,sin-1]*(-rho*dx*dy/4*u[st:en,sin-1]-dy*mu/2) \
+          +v[st-1:en-1,sin-1]*(rho*dx*dy/4*u[st:en,sin-1]+dy*mu/2) \
+          +v[st+1:en+1,sin]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]+dy*mu) \
+          +v[st-1:en-1,sin]*(rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[st:en,sin]**2 \
+          +fdv(dx**2/dy*mu*v[st:en,sin] -dx**2/dy*mu*v[st+fdv:en+fdv,sin] \
+          -dx**2/dy*mu*(v[st+fdv*3:en+fdv*3,sin]-3*v[st+fdv*2:en+fdv*2,sin])))
+        st=-4
+        en=-1
+        fdv=-1
+        pn[st:en,sin]=(dy**2*(p[st+1:en+1,sin]+p[st-1:en-1,sin])+dx**2*(p[st:en,sin+1]+p[st:en,sin-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[st:en,sin+1]-u[st:en,sin-1])**2+rho*dx**2/4*(v[st+1:en+1,sin]-v[st-1:en-1,sin])**2 \
+          +rho*dx*dy/2*(u[st+1:en+1,sin]-u[st-1:en-1,sin])*(v[st:en,sin+1]-v[st:en,sin-1])\
+          +u[st:en,sin+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]+dx*mu)\
+          +u[st:en,sin-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]-dx*mu-dy**2/dx*mu)\
+          +u[st+1:en+1,sin+1]*(rho*dy*dx/4*v[st:en,sin+1]-dx*mu/2) \
+          +u[st+1:en+1,sin-1]*(-rho*dx*dy/4*v[st+1:en+1,sin]-dx*mu/2) \
+          +u[st-1:en-1,sin+1]*(-rho*dy*dx/4*v[st-1:en-1,sin]+dx*mu/2) \
+          +u[st-1:en-1,sin-1]*(rho*dx*dy/4*v[st:en,sin-1]+dx*mu/2) \
+          -2*rho*dy**2*u[st:en,sin]**2 \
+          +fdu*(dy**2/dx*mu*u[st:en,sin] -3*dy**2/dx*mu*u[st:en,sin+fdu]\
+          - dy**2*mu/dx*(u[st:en,sin+fdu*3]-3*u[st:en,sin+fdu*2])) \
+          +v[st+1:en+1,sin+1]*(rho*dx*dy/4*u[st:en,sin+1]-dy*mu/2) \
+          +v[st-1:en-1,sin+1]*(-rho*dx*dy/4*u[st:en,sin+1]+dy*mu/2) \
+          +v[st+1:en+1,sin-1]*(-rho*dx*dy/4*u[st:en,sin-1]-dy*mu/2) \
+          +v[st-1:en-1,sin-1]*(rho*dx*dy/4*u[st:en,sin-1]+dy*mu/2) \
+          +v[st+1:en+1,sin]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]+dy*mu) \
+          +v[st-1:en-1,sin]*(rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[st:en,sin]**2 \
+          +fdv(dx**2/dy*mu*v[st:en,sin] -dx**2/dy*mu*v[st+fdv:en+fdv,sin] \
+          -dx**2/dy*mu*(v[st+fdv*3:en+fdv*3,sin]-3*v[st+fdv*2:en+fdv*2,sin])))
+
         # Poisson equation for pressure (first nodes inside boundary; large y)
+        st=2
+        en=-3
+        sin=-1
+        pn[sin,st:en]=(dy**2*(p[sin+1,st:en]+p[sin-1,st:en])+dx**2*(p[sin,st+1:en+1]+p[sin,st-1:en-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[sin,st+1:en+1]-u[sin,st-1:en-1])**2+rho*dx**2/4*(v[sin+1,st:en]-v[sin-1,st:en])**2 \
+          +rho*dx*dy/2*(u[sin+1,st:en]-u[sin-1,st:en])*(v[sin,st+1:en+1]-v[sin,st-1:en-1])\
+          +u[sin,st+1:en+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]+dx*mu)\
+          +u[sin,st-1:en-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]-dx*mu-dy**2/dx*mu)\
+          +u[sin+1,st+1:en+1]*(rho*dy*dx/4*v[sin,st+1:en+1]-dx*mu/2) \
+          +u[sin+1,st-1:en-1]*(-rho*dx*dy/4*v[sin+1,st:en]-dx*mu/2) \
+          +u[sin-1,st+1:en+1]*(-rho*dy*dx/4*v[sin-1,st:en]+dx*mu/2) \
+          +u[sin-1,st-1:en-1]*(rho*dx*dy/4*v[sin,st-1:en-1]+dx*mu/2) \
+          -2*rho*dy**2*u[sin,st:en]**2 \
+          +fdu*(dy**2/dx*mu*u[sin,st:en] -3*dy**2/dx*mu*u[sin,st+fdu:en+fdu]\
+          - dy**2*mu/dx*(u[sin,st+fdu*3:en+fdu*3]-3*u[sin,st+fdu*2:en+fdu*2])) \
+          +v[sin+1,st+1:en+1]*(rho*dx*dy/4*u[sin,st+1:en+1]-dy*mu/2) \
+          +v[sin-1,st+1:en+1]*(-rho*dx*dy/4*u[sin,st+1:en+1]+dy*mu/2) \
+          +v[sin+1,st-1:en-1]*(-rho*dx*dy/4*u[sin,st-1:en-1]-dy*mu/2) \
+          +v[sin-1,st-1:en-1]*(rho*dx*dy/4*u[sin,st-1:en-1]+dy*mu/2) \
+          +v[sin+1,st:en]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]+dy*mu) \
+          +v[sin-1,st:en]*(rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[sin,st:en]**2 \
+          +fdv(dx**2/dy*mu*v[sin,st:en] -dx**2/dy*mu*v[sin+fdv,st:en] \
+          -dx**2/dy*mu*(v[sin+fdv*3,st:en]-3*v[sin+fdv*2,st:en])))
+        
+        st=-4
+        en=-1
+        fdu=-1
+        pn[sin,st:en]=(dy**2*(p[sin+1,st:en]+p[sin-1,st:en])+dx**2*(p[sin,st+1:en+1]+p[sin,st-1:en-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[sin,st+1:en+1]-u[sin,st-1:en-1])**2+rho*dx**2/4*(v[sin+1,st:en]-v[sin-1,st:en])**2 \
+          +rho*dx*dy/2*(u[sin+1,st:en]-u[sin-1,st:en])*(v[sin,st+1:en+1]-v[sin,st-1:en-1])\
+          +u[sin,st+1:en+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]+dx*mu)\
+          +u[sin,st-1:en-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]-dx*mu-dy**2/dx*mu)\
+          +u[sin+1,st+1:en+1]*(rho*dy*dx/4*v[sin,st+1:en+1]-dx*mu/2) \
+          +u[sin+1,st-1:en-1]*(-rho*dx*dy/4*v[sin+1,st:en]-dx*mu/2) \
+          +u[sin-1,st+1:en+1]*(-rho*dy*dx/4*v[sin-1,st:en]+dx*mu/2) \
+          +u[sin-1,st-1:en-1]*(rho*dx*dy/4*v[sin,st-1:en-1]+dx*mu/2) \
+          -2*rho*dy**2*u[sin,st:en]**2 \
+          +fdu*(dy**2/dx*mu*u[sin,st:en] -3*dy**2/dx*mu*u[sin,st+fdu:en+fdu]\
+          - dy**2*mu/dx*(u[sin,st+fdu*3:en+fdu*3]-3*u[sin,st+fdu*2:en+fdu*2])) \
+          +v[sin+1,st+1:en+1]*(rho*dx*dy/4*u[sin,st+1:en+1]-dy*mu/2) \
+          +v[sin-1,st+1:en+1]*(-rho*dx*dy/4*u[sin,st+1:en+1]+dy*mu/2) \
+          +v[sin+1,st-1:en-1]*(-rho*dx*dy/4*u[sin,st-1:en-1]-dy*mu/2) \
+          +v[sin-1,st-1:en-1]*(rho*dx*dy/4*u[sin,st-1:en-1]+dy*mu/2) \
+          +v[sin+1,st:en]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]+dy*mu) \
+          +v[sin-1,st:en]*(rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[sin,st:en]**2 \
+          +fdv(dx**2/dy*mu*v[sin,st:en] -dx**2/dy*mu*v[sin+fdv,st:en] \
+          -dx**2/dy*mu*(v[sin+fdv*3,st:en]-3*v[sin+fdv*2,st:en])))
+        # Poisson equation for pressure (first nodes inside boundary; large x)
+        st=3
+        en=-2
+        sin=-2
+        pn[st:en,sin]=(dy**2*(p[st+1:en+1,sin]+p[st-1:en-1,sin])+dx**2*(p[st:en,sin+1]+p[st:en,sin-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[st:en,sin+1]-u[st:en,sin-1])**2+rho*dx**2/4*(v[st+1:en+1,sin]-v[st-1:en-1,sin])**2 \
+          +rho*dx*dy/2*(u[st+1:en+1,sin]-u[st-1:en-1,sin])*(v[st:en,sin+1]-v[st:en,sin-1])\
+          +u[st:en,sin+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]+dx*mu)\
+          +u[st:en,sin-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]-dx*mu-dy**2/dx*mu)\
+          +u[st+1:en+1,sin+1]*(rho*dy*dx/4*v[st:en,sin+1]-dx*mu/2) \
+          +u[st+1:en+1,sin-1]*(-rho*dx*dy/4*v[st+1:en+1,sin]-dx*mu/2) \
+          +u[st-1:en-1,sin+1]*(-rho*dy*dx/4*v[st-1:en-1,sin]+dx*mu/2) \
+          +u[st-1:en-1,sin-1]*(rho*dx*dy/4*v[st:en,sin-1]+dx*mu/2) \
+          -2*rho*dy**2*u[st:en,sin]**2 \
+          +fdu*(dy**2/dx*mu*u[st:en,sin] -3*dy**2/dx*mu*u[st:en,sin+fdu]\
+          - dy**2*mu/dx*(u[st:en,sin+fdu*3]-3*u[st:en,sin+fdu*2])) \
+          +v[st+1:en+1,sin+1]*(rho*dx*dy/4*u[st:en,sin+1]-dy*mu/2) \
+          +v[st-1:en-1,sin+1]*(-rho*dx*dy/4*u[st:en,sin+1]+dy*mu/2) \
+          +v[st+1:en+1,sin-1]*(-rho*dx*dy/4*u[st:en,sin-1]-dy*mu/2) \
+          +v[st-1:en-1,sin-1]*(rho*dx*dy/4*u[st:en,sin-1]+dy*mu/2) \
+          +v[st+1:en+1,sin]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]+dy*mu) \
+          +v[st-1:en-1,sin]*(rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[st:en,sin]**2 \
+          +fdv(dx**2/dy*mu*v[st:en,sin] -dx**2/dy*mu*v[st+fdv:en+fdv,sin] \
+          -dx**2/dy*mu*(v[st+fdv*3:en+fdv*3,sin]-3*v[st+fdv*2:en+fdv*2,sin])))  
+        
+        st=1
+        en=4
+        fdv=1
+        pn[st:en,sin]=(dy**2*(p[st+1:en+1,sin]+p[st-1:en-1,sin])+dx**2*(p[st:en,sin+1]+p[st:en,sin-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[st:en,sin+1]-u[st:en,sin-1])**2+rho*dx**2/4*(v[st+1:en+1,sin]-v[st-1:en-1,sin])**2 \
+          +rho*dx*dy/2*(u[st+1:en+1,sin]-u[st-1:en-1,sin])*(v[st:en,sin+1]-v[st:en,sin-1])\
+          +u[st:en,sin+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]+dx*mu)\
+          +u[st:en,sin-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[st:en,sin]-dx*mu-dy**2/dx*mu)\
+          +u[st+1:en+1,sin+1]*(rho*dy*dx/4*v[st:en,sin+1]-dx*mu/2) \
+          +u[st+1:en+1,sin-1]*(-rho*dx*dy/4*v[st+1:en+1,sin]-dx*mu/2) \
+          +u[st-1:en-1,sin+1]*(-rho*dy*dx/4*v[st-1:en-1,sin]+dx*mu/2) \
+          +u[st-1:en-1,sin-1]*(rho*dx*dy/4*v[st:en,sin-1]+dx*mu/2) \
+          -2*rho*dy**2*u[st:en,sin]**2 \
+          +fdu*(dy**2/dx*mu*u[st:en,sin] -3*dy**2/dx*mu*u[st:en,sin+fdu]\
+          - dy**2*mu/dx*(u[st:en,sin+fdu*3]-3*u[st:en,sin+fdu*2])) \
+          +v[st+1:en+1,sin+1]*(rho*dx*dy/4*u[st:en,sin+1]-dy*mu/2) \
+          +v[st-1:en-1,sin+1]*(-rho*dx*dy/4*u[st:en,sin+1]+dy*mu/2) \
+          +v[st+1:en+1,sin-1]*(-rho*dx*dy/4*u[st:en,sin-1]-dy*mu/2) \
+          +v[st-1:en-1,sin-1]*(rho*dx*dy/4*u[st:en,sin-1]+dy*mu/2) \
+          +v[st+1:en+1,sin]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]+dy*mu) \
+          +v[st-1:en-1,sin]*(rho*dx**2*dy/2/dt+rho*dx**2*v[st:en,sin]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[st:en,sin]**2 \
+          +fdv(dx**2/dy*mu*v[st:en,sin] -dx**2/dy*mu*v[st+fdv:en+fdv,sin] \
+          -dx**2/dy*mu*(v[st+fdv*3:en+fdv*3,sin]-3*v[st+fdv*2:en+fdv*2,sin])))
+        # Poisson equation for pressure (first nodes inside boundary; small y)
+        st=3
+        en=-2
+        sin=1
+        pn[sin,st:en]=(dy**2*(p[sin+1,st:en]+p[sin-1,st:en])+dx**2*(p[sin,st+1:en+1]+p[sin,st-1:en-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[sin,st+1:en+1]-u[sin,st-1:en-1])**2+rho*dx**2/4*(v[sin+1,st:en]-v[sin-1,st:en])**2 \
+          +rho*dx*dy/2*(u[sin+1,st:en]-u[sin-1,st:en])*(v[sin,st+1:en+1]-v[sin,st-1:en-1])\
+          +u[sin,st+1:en+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]+dx*mu)\
+          +u[sin,st-1:en-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]-dx*mu-dy**2/dx*mu)\
+          +u[sin+1,st+1:en+1]*(rho*dy*dx/4*v[sin,st+1:en+1]-dx*mu/2) \
+          +u[sin+1,st-1:en-1]*(-rho*dx*dy/4*v[sin+1,st:en]-dx*mu/2) \
+          +u[sin-1,st+1:en+1]*(-rho*dy*dx/4*v[sin-1,st:en]+dx*mu/2) \
+          +u[sin-1,st-1:en-1]*(rho*dx*dy/4*v[sin,st-1:en-1]+dx*mu/2) \
+          -2*rho*dy**2*u[sin,st:en]**2 \
+          +fdu*(dy**2/dx*mu*u[sin,st:en] -3*dy**2/dx*mu*u[sin,st+fdu:en+fdu]\
+          - dy**2*mu/dx*(u[sin,st+fdu*3:en+fdu*3]-3*u[sin,st+fdu*2:en+fdu*2])) \
+          +v[sin+1,st+1:en+1]*(rho*dx*dy/4*u[sin,st+1:en+1]-dy*mu/2) \
+          +v[sin-1,st+1:en+1]*(-rho*dx*dy/4*u[sin,st+1:en+1]+dy*mu/2) \
+          +v[sin+1,st-1:en-1]*(-rho*dx*dy/4*u[sin,st-1:en-1]-dy*mu/2) \
+          +v[sin-1,st-1:en-1]*(rho*dx*dy/4*u[sin,st-1:en-1]+dy*mu/2) \
+          +v[sin+1,st:en]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]+dy*mu) \
+          +v[sin-1,st:en]*(rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[sin,st:en]**2 \
+          +fdv(dx**2/dy*mu*v[sin,st:en] -dx**2/dy*mu*v[sin+fdv,st:en] \
+          -dx**2/dy*mu*(v[sin+fdv*3,st:en]-3*v[sin+fdv*2,st:en])))
+        
+        st=2
+        en=3
+        fdu=1
+        pn[sin,st:en]=(dy**2*(p[sin+1,st:en]+p[sin-1,st:en])+dx**2*(p[sin,st+1:en+1]+p[sin,st-1:en-1]))/(2*(dx**2+dy**2)) \
+          +(1/(2*(dx**2+dy**2)))*(rho*dy**2/4*(u[sin,st+1:en+1]-u[sin,st-1:en-1])**2+rho*dx**2/4*(v[sin+1,st:en]-v[sin-1,st:en])**2 \
+          +rho*dx*dy/2*(u[sin+1,st:en]-u[sin-1,st:en])*(v[sin,st+1:en+1]-v[sin,st-1:en-1])\
+          +u[sin,st+1:en+1]*(-rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]+dx*mu)\
+          +u[sin,st-1:en-1]*(rho*dx*dy**2/2/dt+rho*dy**2*u[sin,st:en]-dx*mu-dy**2/dx*mu)\
+          +u[sin+1,st+1:en+1]*(rho*dy*dx/4*v[sin,st+1:en+1]-dx*mu/2) \
+          +u[sin+1,st-1:en-1]*(-rho*dx*dy/4*v[sin+1,st:en]-dx*mu/2) \
+          +u[sin-1,st+1:en+1]*(-rho*dy*dx/4*v[sin-1,st:en]+dx*mu/2) \
+          +u[sin-1,st-1:en-1]*(rho*dx*dy/4*v[sin,st-1:en-1]+dx*mu/2) \
+          -2*rho*dy**2*u[sin,st:en]**2 \
+          +fdu*(dy**2/dx*mu*u[sin,st:en] -3*dy**2/dx*mu*u[sin,st+fdu:en+fdu]\
+          - dy**2*mu/dx*(u[sin,st+fdu*3:en+fdu*3]-3*u[sin,st+fdu*2:en+fdu*2])) \
+          +v[sin+1,st+1:en+1]*(rho*dx*dy/4*u[sin,st+1:en+1]-dy*mu/2) \
+          +v[sin-1,st+1:en+1]*(-rho*dx*dy/4*u[sin,st+1:en+1]+dy*mu/2) \
+          +v[sin+1,st-1:en-1]*(-rho*dx*dy/4*u[sin,st-1:en-1]-dy*mu/2) \
+          +v[sin-1,st-1:en-1]*(rho*dx*dy/4*u[sin,st-1:en-1]+dy*mu/2) \
+          +v[sin+1,st:en]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]+dy*mu) \
+          +v[sin-1,st:en]*(rho*dx**2*dy/2/dt+rho*dx**2*v[sin,st:en]-dy*mu-dx**2/dy*mu) \
+          -2*dx**2*v[sin,st:en]**2 \
+          +fdv(dx**2/dy*mu*v[sin,st:en] -dx**2/dy*mu*v[sin+fdv,st:en] \
+          -dx**2/dy*mu*(v[sin+fdv*3,st:en]-3*v[sin+fdv*2,st:en])))
         
         # Poisson equation for pressure (main)
         pn[2:-2,2:-2]=(dx**2*(p[3:-1,2:-2]+p[1:-3,2:-2])+dy**2*(p[2:-2,3:-1]+p[2:-2,1:-3]))/(2*(dx**2+dy**2)) \
@@ -104,14 +262,23 @@ def ResolvePress(p, u, v, dxyt, prop, conv, dp_zero):
           +v[1:-3,1:-3]*(rho*dx*dy/4*u[2:-2,1:-3]+dy*mu/2) \
           +v[3:-1,2:-2]*(-rho*dx**2*dy/2/dt+rho*dx**2*v[2:-2,2:-2]+dy*mu+dx**2/dy*mu) \
           +v[1:-3,2:-2]*(rho*dx**2*dy/2/dt+rho*dx**2*v[2:-2,2:-2]-dy*mu-dx**2/dy*mu) \
-          -2*dx**2*v[2:-2,2:-2]**2 + dx**2/2/dy*mu*(v[:-4,2:-2]-v[4:,2:-2]))
+          -2*rho*dx**2*v[2:-2,2:-2]**2 + dx**2/2/dy*mu*(v[:-4,2:-2]-v[4:,2:-2]))
         
+        # Enforce dp=0 BCs
+        if dp_zero[0]==1:
+            p[1:-1,0]=p[1:-1,1]
+        if dp_zero[1]==1:
+            p[1:-1,-1]=p[1:-1,-2]
+        if dp_zero[2]==1:
+            p[0,1:-1]=p[1,1:-1]
+        if dp_zero[3]==1:
+            p[-1,1:-1]=p[-2,1:-1]
           # Convergence check
         diff=numpy.sum(numpy.abs(p[:]-pn[:]))/numpy.sum(numpy.abs(p[:]))
         p=pn.copy()
         count=count+1
     if count==1000:
-        print 'Convergence problem for pressure distribution'
+        print 'Convergence problems resolving the pressure distribution'
         error=1
     return p,error
 
@@ -145,7 +312,9 @@ for i in range(Nt):
     
     # Solve pressure field
     p,error=ResolvePress(p, u, v, (dx, dy, dt), (rho, mu), conv)
-    
+    if error==1:
+        print 'Run aborted at time step %i'%i
+        break
     # Solve momentum equations (explicit, first order for time)
     un=u.copy()
     un[1:-1,1:-1]=dt/(2*rho*dx)*(p[1:-1,:-2]+p[1:-1,2:]) \
